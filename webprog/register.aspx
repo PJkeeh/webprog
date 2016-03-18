e5ca22fe1993ad162baf608c37cc4d73d873db70
PJkeeh <r0415320@student.vives.be>
2016-03-18 02:02:04 +0100

    Added register page

---
+++ webprog/register.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage/Site1.Master" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="webprog.register" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form runat="server">
+    <p>
+        <label class="registerLabel">Login:</label>
+        <asp:TextBox ID="txtLogin" runat="server"></asp:TextBox>
+        <label id="lblLoginError" runat="server" class="errorLabel"></label>
+    </p>
+    <p>
+       <label class="registerLabel">Wachtwoord:</label>
+       <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+       <label id="lblPasswordError" runat="server" class="errorLabel"></label>
+    </p>
+    <p>
+       <label class="registerLabel">Wachtwoord herhalen:</label>
+       <asp:TextBox ID="txtPassword2" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btnRegister" runat="server" Text="Button" OnClick="btnRegister_Click" />
+    </p>
+    </form>
+</asp:Content>
---
+++ webprog/register.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Security;
+
+namespace webprog
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            LoginService loginService = new LoginService();
+
+            if (txtLogin.Text.Trim().Equals(""))
+            {
+                lblLoginError.InnerHtml = "Geef een gebruikersnaam in.";
+            }
+            else {
+                if (loginService.getLogin(txtLogin.Text) == null)
+                {
+                    lblLoginError.InnerHtml = "";
+                    if (txtPassword.Text.Equals(txtPassword2.Text))
+                    {
+                        lblPasswordError.InnerHtml = "";
+                        if (txtPassword.Text.Equals(""))
+                        {
+                            lblPasswordError.InnerHtml = "Geef een wachtwoord in.";
+                        }
+                        else
+                        {
+                            lblPasswordError.InnerHtml = "";
+
+#pragma warning disable CS0618 // Type or member is obsolete
+                            loginService.registerLogin(txtLogin.Text, FormsAuthentication.HashPasswordForStoringInConfigFile(txtPassword.Text, "SHA1"));
+#pragma warning restore CS0618 // Type or member is obsolete
+                            Response.Redirect("index.aspx");
+                        }
+                    }
+                    else
+                    {
+                        lblPasswordError.InnerHtml = "De wachtwoorden komen niet overeen.";
+                    }
+                }
+                else
+                {
+                    lblLoginError.InnerHtml = "Deze gebruikersnaam is al in gebruik.";
+                }
+            }
+        }
+    }
+}
