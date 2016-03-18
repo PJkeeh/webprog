f070145fd8482ce80a72b8e517bee9df5e4c7c3e
PJkeeh <r0415320@student.vives.be>
2016-03-18 02:02:22 +0100

    Fixed Navbar 'active' state

---
+++ webprog/js/navbar.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var url = window.location;
+    $('.menu').find('.active').removeClass('active');
+    $('.menu li a').each(function () {
+        if (this.href == url) {
+            $(this).addClass('active');
+        }
+    });
+});
