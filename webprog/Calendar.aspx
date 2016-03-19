cefca8598d55503fefcb8d4b35f7f0b036f918dc
PJkeeh <pieterv2000@hotmail.com>
2016-03-19 23:42:33 +0100

    Added Calendar page

---
+++ webprog/Calendar.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage/Site1.Master" AutoEventWireup="true" CodeBehind="Calendar.aspx.cs" Inherits="webprog.Calendar" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="matchesID" runat="server" class="leftContent">
+
+    </div>
+    <div id="filter" class="rightContent">
+        <div>CalenderFilterPlaceholder</div>
+        <div>ClubFilterPlaceholder</div>
+    </div>
+</asp:Content>
---
+++ webprog/Calendar.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webprog
+{
+    public partial class Calendar : System.Web.UI.Page
+    {
+        MatchService matchService = new MatchService();
+        List<Match> matches;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String club = Request.QueryString["club"];
+            int selected;
+
+            if(club == null || club.Equals("") || Int32.TryParse(club, out selected) == false)
+            {
+                matches = matchService.getAllMatches();
+            }
+            else
+            {
+                matches = matchService.getAllMatchesOfTeam(selected);
+            }
+
+            fillMatchesDiv(matches);
+        }
+
+        private void fillMatchesDiv(List<Match> matches)
+        {
+            if (matches.Count == 0)
+            {
+                matchesID.InnerHtml += "<p>Geen matches gepland.</p>";
+            }
+            else
+            {
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    if (i == 5)
+                        break;
+                    matchesID.InnerHtml += "<h2>" + matches.ElementAt(i).homeTeam.name + " - " + matches.ElementAt(i).awayTeam.name + "</h2>";
+                    matchesID.InnerHtml += "<p>" + matches.ElementAt(i).date.ToShortDateString() + "</p>";
+                }
+            }
+        }
+    }
+}
