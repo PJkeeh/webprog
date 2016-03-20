8b24afb594822241e4c3a44f304a75b29966024a
PJkeeh <pieterv2000@hotmail.com>
2016-03-20 19:51:41 +0100

    Fixed a bug where parameters and hashes caused the script to misbehave.

--- webprog/js/navbar.js
+++ webprog/js/navbar.js
@@ -1,9 +1,11 @@
 ﻿$(document).ready(function () {
-    var url = window.location;
+    var url = window.location.href;
     $('.menu').find('.active').removeClass('active');
     $('.menu li a').each(function () {
-        if (this.href == url) {
+        if (this.href == url.split(/[?#]/)[0]) {
             $(this).addClass('active');
+        } else {
+            console.log(url.split(/[?#]/)[0]);
         }
     });
 });
