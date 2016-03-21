f6e46d90740c65de5812eee6938bace81d4ade78
PJkeeh <r0415320@student.vives.be>
2016-03-21 22:38:39 +0100

    Added Abonnement

---
+++ webprog/DAO/AbonnementDAO.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using webprog.Domain;
+
+namespace webprog.DAO
+{
+    public class AbonnementDAO
+    {
+        private String dbLoc = ConfigurationManager.ConnectionStrings["webprog"].ConnectionString;
+        private SqlConnection cnn;
+        SqlDataReader reader;
+
+        public AbonnementDAO() { }
+        
+        public List<Abonnement> getAll()
+        {
+            cnn = new SqlConnection(dbLoc);
+            List<Abonnement> retVal = new List<Abonnement>();
+
+            String strSQL = "SELECT * FROM abonnement;";
+
+            SqlCommand com = new SqlCommand(strSQL, cnn);
+
+            try
+            {
+                cnn.Open();
+                reader = com.ExecuteReader();
+
+                // Call Read before accessing data
+                while (reader.Read())
+                { retVal.Add(CreateAbonnement(reader)); }
+
+                // Call close when done reading
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Something went wrong ", ex);
+            }
+            finally
+            {
+                cnn.Close();
+                reader.Close();
+            }
+        }
+
+        public List<Abonnement> getAllOfTeam(int team_id, DateTime seasonDate)
+        {
+            cnn = new SqlConnection(dbLoc);
+            List<Abonnement> retVal = new List<Abonnement>();
+
+            String strSQL = "SELECT * FROM abonnement where abo_team_id=@team_id and abo_start_date<=@seasonDate and abo_end_date>@seasonDate;";
+
+            SqlCommand com = new SqlCommand(strSQL, cnn);
+            com.Parameters.AddWithValue("@team_id", team_id);
+            com.Parameters.AddWithValue("@seasonDate", seasonDate.Date);
+
+            try
+            {
+                cnn.Open();
+                reader = com.ExecuteReader();
+
+                // Call Read before accessing data
+                while (reader.Read())
+                { retVal.Add(CreateAbonnement(reader)); }
+
+                // Call close when done reading
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Something went wrong ", ex);
+            }
+            finally
+            {
+                cnn.Close();
+                reader.Close();
+            }
+        }
+
+        public Abonnement CreateAbonnement(SqlDataReader reader)
+        {
+            ClubDAO clubDAO = new ClubDAO();
+            LoginDAO loginDAO = new LoginDAO();
+            Ticket_typeDAO ticket_typeDAO = new Ticket_typeDAO();
+
+            return new Abonnement
+            {
+                abo_id = Convert.ToInt32(reader["abo_id"]),
+                club = clubDAO.getClub(Convert.ToInt32(reader["abo_team_id"])),
+                login = loginDAO.getLogin(Convert.ToString(reader["abo_login"])),
+                startDate = Convert.ToDateTime(reader["abo_start_date"]),
+                endDate = Convert.ToDateTime(reader["abo_end_date"]),
+                ticket_type = ticket_typeDAO.getTicket_type(Convert.ToInt32(reader["abo_ticket_type"])),
+            };
+        }
+    }
+}
---
+++ webprog/Domain/abonnement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webprog.Domain
+{
+    public class Abonnement
+    {
+        public int abo_id { get; set; }
+
+        public Club club { get; set; }
+
+        public Login login { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+
+        public Ticket_type ticket_type { get; set; }
+    }
+}
