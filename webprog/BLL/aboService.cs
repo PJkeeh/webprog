d534098c7e3adb8057620ef23a98cc12c607e6a2
PJkeeh <r0415320@student.vives.be>
2016-03-22 00:38:01 +0100

    Added AboService

---
+++ webprog/BLL/aboService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webprog.DAO;
+using webprog.Domain;
+
+namespace webprog.BLL
+{
+    public class AboService
+    {
+        AbonnementDAO dao = new AbonnementDAO();
+
+        public List<Abonnement> getAll()
+        {
+            return dao.getAll();
+        }
+
+        public List<Abonnement> getAllOfTeam(int id)
+        {
+            return dao.getAllOfTeam(id);
+        }
+
+        public List<Abonnement> getTicket_type(int id)
+        {
+            return new AbonnementDAO().getTicket_type(id);
+        }
+        
+    }
+}
