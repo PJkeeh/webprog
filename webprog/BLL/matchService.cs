18bdaa8136e82a869ee7f52fe5171f8aebfac576
PJkeeh <pieterv2000@hotmail.com>
2016-03-21 01:09:48 +0100

    Added getMatch() Added getTicketsAvailable()

--- webprog/BLL/matchService.cs
+++ webprog/BLL/matchService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using webprog.Domain;
 using webprog.DAO;
+using System.Linq;
 
 namespace webprog.BLL
 {
@@ -11,6 +12,11 @@ namespace webprog.BLL
 
         public MatchService()
         {
+        }
+
+        public Match getMatch(int id)
+        {
+            return dao.getMatch(id);
         }
 
         public List<Match> getAllComing()
@@ -31,6 +37,38 @@ namespace webprog.BLL
         public List<Match> getAllMatchesOfTeam(int id)
         {
             return dao.getAllOfTeam(id);
+        }
+        
+        public List<int[]> getTicketsAvailable(Match m)
+        {
+            List<int[]> retVal = new List<int[]>();
+
+            Ticket_teamDAO ticket_teamDAO = new Ticket_teamDAO();
+            List<Ticket_team> ticket_team = ticket_teamDAO.getAllOfTeam(m.homeTeam.id);
+
+            TicketDAO ticketDAO = new TicketDAO();
+            List<Ticket> tickets = ticketDAO.getAllOfMatch(m);
+
+            for (int i = 0; i < ticket_team.Count; i++)
+            {
+                int total = ticket_team.ElementAt(i).amount;
+                int sold = 0;
+                for (int j = 0; j < tickets.Count; j++)
+                {
+                    if (tickets.ElementAt(j) == null)
+                    {
+                        break;
+                    }
+                    else if (tickets.ElementAt(j).ticket_type == ticket_team.ElementAt(i).ticket_type)
+                    {
+                        sold++;
+                        break;
+                    }
+                }
+                int[] a = { ticket_team.ElementAt(i).ticket_type.id, sold, total };
+                retVal.Add(a);
+            }
+            return retVal;
         }
     }
 }
