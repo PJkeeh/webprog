ba8e4bbad0ec0f30e787abe93794d544d9385b3d
PJkeeh <pieterv2000@hotmail.com>
2016-03-21 01:08:47 +0100

    created LoggedIn()

---
+++ webprog/masterpageFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using webprog.BLL;
+
+namespace webprog
+{
+    public class masterpageFunctions : System.Web.UI.MasterPage
+    {
+        public Boolean loggedIn()
+        {
+            LoginService loginService = new LoginService();
+            return !(Session["username"] != null
+                && !((string)Session["username"]).Equals("")
+                && loginService.getLogin((string)Session["username"]) != null);
+        }
+    }
+}
