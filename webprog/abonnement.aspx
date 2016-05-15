d5a5205706269a6821b8d136b2666fa7ba501712
PJkeeh <r0415320@student.vives.be>
2016-05-15 23:40:37 +0200

    Added Abonnement page code

--- webprog/abonnement.aspx
+++ webprog/abonnement.aspx
@@ -1,3 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage/Site1.Master" AutoEventWireup="true" CodeBehind="abonnement.aspx.cs" Inherits="webprog.Abo" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content" id="abo_this_season" runat="server"></div>
+    <div class="content" id="abo_next_season" runat="server"></div>
 </asp:Content>
--- webprog/abonnement.aspx.cs
+++ webprog/abonnement.aspx.cs
@@ -1,17 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using webprog.BLL;
+using webprog.Domain;
 
 namespace webprog
 {
-    public partial class Abo : System.Web.UI.Page
+    public partial class Abo : pageFunctions
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!loggedIn())
+            {
+                Response.Redirect("login.aspx");
+            }
+            else
+            {
+                LoginService loginservice = new LoginService();
+                Login login = loginservice.getLogin(getLogin());
+                if (login == null)
+                {
+                    Response.Redirect("login.aspx");
+                }
+                else
+                {
+                    SeizoenService seizoenService = new SeizoenService();
+                    Seizoen seizoen_current = seizoenService.getCurrent();
+                    Seizoen seizoen_next = seizoenService.getNext();
 
+                    AboService aboService = new AboService();
+
+                    if (seizoen_next != null)
+                    {
+                        Abonnement abo_next = aboService.getAbonnement(login, seizoen_next);
+                        abo_next_season.InnerHtml = "<h1>" + seizoen_next + "</h1><p>Het volgende seizoen start op " + string.Format("{0:dd-MM-yyyy}", seizoen_next.startDate.Date) + ".";
+                        if(abo_next != null)
+                        {
+                            abo_next_season.InnerHtml += "<p>Je hebt al een ticket voor dit seizoen voor " + abo_next.club.name + ".";
+                        }
+                        else
+                        {
+                            abo_next_season.InnerHtml += "<p><a href='aboBuy.aspx'>Klik hier</a> om een abonnement te kopen voor jouw club.";
+                        }
+                    }
+                    if (seizoen_current != null)
+                    {
+                        Abonnement abo_now = aboService.getAbonnement(login, seizoen_current);
+                        abo_this_season.InnerHtml = "<h1>" + seizoen_current + "</h1>";
+                        if (abo_now != null)
+                            abo_this_season.InnerHtml += "<p>Je hebt een abonnement voor " + abo_now.club.name + ". Dit abonnement vervalt op " + string.Format("{0:dd-MM-yyyy}", seizoen_current.endDate.Date) + ".";
+                        else
+                            abo_this_season.InnerHtml += "<p>Je kan voor dit seizoen geen abonnement meer verkrijgen. Voor losse tickets, ga naar de <a href='calendar.aspx'>Kalender</a></p>";
+                    }
+                }
+            }
         }
     }
 }
